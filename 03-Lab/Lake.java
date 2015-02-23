003949878cf97355dce52dfcd137f9fb431111bd
Sarah Chen <sarah.chen@cslab1>
2015-02-23 08:39:24 -0500

    lab3

---
+++ 03-Lab/Lake.java
@@ -0,0 +1,16 @@
+
+public class Lake {
+    private int[][] Lake;
+    private int[] Given;
+    private Random r;
+    
+    public Lake() {
+        Given = [4, 6, 22, 2];
+	Lake = int[Given.get(0)][Given.get(1)];
+	
+	for(int r=0;r<Lake.length;r++) {
+	    
+	}
+	
+    }
+}
