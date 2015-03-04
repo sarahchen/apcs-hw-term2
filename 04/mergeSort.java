0f7fb82cee67b9f996be5a81747c911837767ba2
Sarah Chen <sarah.chen@cslab1>
2015-03-04 08:41:54 -0500

    Update

--- 04/mergeSort.java
+++ 04/mergeSort.java
@@ -1,30 +1,46 @@
+import java.util.*;
+import java.io.*;
 
 public class mergeSort {
     private ArrayList<Integer> A;
     private ArrayList<Integer> B;
+    private Random rng;
 
     public mergeSort() {
+	rng = new Random();
 
+	A = new ArrayList<Integer>();
+	B = new ArrayList<Integer>();
+
+	for(int i=0;i<20;i++) {
+	    A.add(rng.nextInt(20));
+	    B.add(rng.nextInt(20));
+	}
     }
 
-    public ArrayList<Integer> merge(ArrayList<Integer> A, ArrayList<Integer> B) {
+    public void sort() {
+	
+    }
+
+    public ArrayList<Integer> merge(ArrayList<Integer> one, ArrayList<Integer> two) {
 	ArrayList<Integer> M = new ArrayList<Integer>();
-	while(A.size() > 0 || B.size() > 0) {
-	    if(A.size() == 0) {
-	        while(B.size() > 0) {
-		    M.add(B.remove(0));
+	while(one.size() > 0 || two.size() > 0) {
+	    if(one.size() == 0) {
+	        while(two.size() > 0) {
+		    M.add(two.remove(0));
 		}
 	    }
-	    else if(B.size() == 0) {
-		while(A.size() > 0) {
-		    M.add(A.remove(0));
+	    else if(two.size() == 0) {
+		while(one.size() > 0) {
+		    M.add(one.remove(0));
 		}
 	    }
-	    else if(A.get(0) > B.get(0)) {
-		M.add(A.remove(0));
+	    else if(one.get(0) > two.get(0)) {
+		M.add(one.remove(0));
 	    } else {
-		M.add(B.remove(0));
+		M.add(two.remove(0));
 	    }
 	}
+	return M;
     }
 }
