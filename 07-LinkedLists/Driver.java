c5327e5dca1f8d48e38ab535d261296c0536d26f
Sarah Chen <sarah.chen@cslab1>
2015-03-12 08:39:53 -0400

    Linked List

--- 07-LinkedLists/Driver.java
+++ 07-LinkedLists/Driver.java
@@ -5,7 +5,14 @@ public class Driver {
 	Node n2 = new Node("world");
 	
 	n.setNext(n2);
-	System.out.println(n.getNext().getData());
+	//System.out.println(n.getNext().getData());
+
+	n2.setNext(new Node("watermelon"));
+	n2.getNext().setNext(new Node("abc"));
+
+	for(Node tmp = n; tmp!=null; tmp = tmp.getNext()) {
+	    System.out.println(tmp.getData());
+	}
     }
 
 }
