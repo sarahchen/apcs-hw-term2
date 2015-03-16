34e75db6c9824916e3602e5a3b09058fb856d360
Sarah Chen <sarah.chen@cslab1>
2015-03-16 08:32:37 -0400

    Finish 07

---
+++ 07-lists1/LList.java
@@ -0,0 +1,46 @@
+public class LList {
+    private Node l=null;
+
+    public String toString() {
+	String s = "";
+	Node tmp;
+	for(tmp = l;tmp != null; tmp=tmp.getNext()) {
+	    s = s + tmp + " ---> ";
+	}
+	s = s + "null";
+	return s;
+    }
+
+    // Adds a node to the beginning of the list
+    public void add(String s) {
+        Node tmp = new Node(s);
+	tmp.setNext(l);
+	l = tmp;
+    }
+
+    // returns the data of the Node at index n
+    public String find(int n) {
+        Node lychee = l;
+	for(int i=0;i<n;i++) {
+	    lychee = lychee.getNext();
+	}
+	return lychee.getData();
+    }
+
+    // returns the Node at index n
+    public Node findN(int n) {
+	Node dragon = l;
+	for(int i=0;i<n;i++) {
+	    dragon = dragon.getNext();
+	}
+	return dragon;
+    }
+
+    // inserts new node at index n
+    public void insert(int n, String s) {
+	Node star = new Node(s);
+	star.setNext(findN(n));
+	findN(n-1).setNext(star);
+    }
+
+}
