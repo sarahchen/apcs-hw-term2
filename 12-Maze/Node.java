32c831210865bce82581fc6c7d139b12a4dc7c99
Sarah Chen <sarah.chen@cslab1>
2015-04-01 08:41:47 -0400

    Breadth Firsth Search

---
+++ 12-Maze/Node.java
@@ -0,0 +1,30 @@
+public class Node<E> {
+    private E data;
+    private Node<E> next = null;
+
+    public Node() {
+    }
+    public Node(E data) {
+	this.data = data;
+    }
+    public Node(E data, Node<E> next) {
+	this.data = data;
+	this.next = next;
+    }
+
+    public E getData(){
+	return data;
+    }
+
+    public void setData(E data) {
+	this.data = data;
+    }
+
+    public Node<E> getNext() {
+	return next;
+    }
+
+    public void setNext(Node<E> n) {
+	next = n;
+    }
+}
---
+++ 12-Maze/Queue.java
@@ -0,0 +1,49 @@
+
+public class Queue<E> {
+    private Node<E> first; // Dummy Node (Stays in beginning)
+    private Node<E> last; // Keeps track of last Node
+
+    public Queue() {
+	first = new Node<E>();
+	last = first;
+    }
+
+    public void enqueue(E data) {
+	Node<E> n = new Node<E>(data);
+	last.setNext(n);
+	last = n;
+    }
+
+    public E dequeue() {
+	Node<E> tmp = first.getNext();
+	first.setNext(first.getNext().getNext());
+	return tmp.getData();
+    }
+
+    public String toString() {
+	String s = "";
+	Node<E> yay = first;
+	while(yay!=last) {
+	    s = s + yay.getData() + ", ";
+	    yay = yay.getNext();
+	}
+	s = s + last.getData();
+	return s;
+    }
+
+    public static void main(String[] args) {
+	Queue<Integer> q = new Queue<Integer>();
+	for(int i=0; i<10; i++) {
+	    q.enqueue(i);
+	}
+	System.out.println(q);
+	for(int i=0;i<3;i++) {
+	    q.dequeue();
+	}
+	System.out.println(q);
+	q.enqueue(34);
+	System.out.println(q);
+	
+    }
+
+}
